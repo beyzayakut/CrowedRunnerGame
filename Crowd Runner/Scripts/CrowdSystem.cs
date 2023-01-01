d66e06b37235d760b446624fba722756d5919f5f
beyzayakut <93875186+beyzayakut@users.noreply.github.com>
2023-01-01 20:38:41 +0300

    Add files via upload

---
+++ Crowd Runner/Scripts/CrowdSystem.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdSystem : MonoBehaviour
+{
+    [Header("Elements ")]
+    [SerializeField] private Transform runnersParent;
+
+
+    [Header("Settings ")]
+    [SerializeField] private float radius;
+    [SerializeField] private float angle;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlaceRunners();
+    }
+
+    private void PlaceRunners()
+    {
+
+        for (int i = 0; i < runnersParent.childCount; i++)
+        {
+            Vector3 childLocalPosition = GetRunnerLocalPosition(i);
+            runnersParent.GetChild(i).localPosition = childLocalPosition;
+
+        }
+    }
+
+    private Vector3 GetRunnerLocalPosition(int index)
+    {
+        float x = radius * Mathf.Sqrt(index) * Mathf.Cos(Mathf.Deg2Rad * index * angle);
+        float z = radius * Mathf.Sqrt(index) * Mathf.Sin(Mathf.Deg2Rad * index * angle);
+
+        return new Vector3(x, 0, z);
+
+    }
+
+    public float GetCrowdRadius()
+    {
+        return radius * Mathf.Sqrt(runnersParent.childCount);
+    }
+
+}
